e1a1477fa4ecc02cf8fbed5ff05f7ca6401199d2
Kate <kowens711@gmail.com>
2015-10-08 13:59:47 -0400

    made class of contactlist

--- ContactList/ContactList.swift
+++ ContactList/ContactList.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+class ContactList {
+    var name: String
+    var contacts: [Contact]
+    
+    init(name: String) {
+        self.name = name
+        self.contacts = []
+    }
+}
